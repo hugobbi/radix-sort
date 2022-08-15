5da3016e5d188933844d14a8e0013de9751997c5
hugobbi <hgugobbi@gmail.com>
2022-08-15 18:23:11 -0300

    aa

--- radix.py
+++ radix.py
@@ -6,9 +6,11 @@ def max_char(input):
     max_char = max(map(len, input))
     return max_char
 
-def radix_sort(input, base, hi, lo, pos):
+def radix_sort(input, base, max_char, pos):
+    if pos == max_char:
+      return input
     n = len(input)
-    ordered_input = []
+    ordered_input = [0]*n
     count_vector = [0]*base
     conv_base = ord("A")
 
@@ -26,14 +28,16 @@ def radix_sort(input, base, hi, lo, pos):
     for word in reversed(input):
         if pos < len(word):
             pos_letter = ord(word[pos]) - conv_base
-        ordered_input[count_vector[pos_letter]] = word
+        ordered_input[count_vector[pos_letter]-1] = word
         count_vector[pos_letter] -= 1
 
     print(count_vector)
     print(ordered_input)
-    return ordered_input
+    pos += 1
+    radix_sort(ordered_input, base, max_char, pos)
 
 input = ["OLA", "JORGE", "KA", "ABC"]
 base = 26
 
-radix_sort(input, base, 1, 2, 0)
+max_char = max_char(input)
+radix_sort(input, base, max_char, 0)
